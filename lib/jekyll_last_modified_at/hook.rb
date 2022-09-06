a80e36b7749ab051a8026f27d82d5ac44e25bdb8
Anthony Ross <anthony.s.ross@gmail.com>
2022-09-06 09:08:15 -0700

    Rename var

--- lib/jekyll_last_modified_at/hook.rb
+++ lib/jekyll_last_modified_at/hook.rb
@@ -4,9 +4,9 @@ module JekyllLastModifiedAt
   module Hook
     def self.get_modified
       proc do |item|
-        modified_at = JekyllLastModifiedAt::Loader.new(doc).last_modified_at
+        modified_at = JekyllLastModifiedAt::Loader.new(item).last_modified_at
 
-        doc.data['last_modified_at'] = modified_at
+        item.data['last_modified_at'] = modified_at
       end
     end
   end
